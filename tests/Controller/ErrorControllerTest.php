ffea0bee4bec1df62ada7a2d070ff1fbc3a3cff4
BarroisGregory <gregorybarrois@laposte.net>
2018-06-27 13:48:06 +0200

    test page 404

---
+++ tests/Controller/ErrorControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ErrorControllerTest extends WebTestCase
+{
+
+    public function test404()
+    {
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/coucou');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode());
+
+    }
+}
